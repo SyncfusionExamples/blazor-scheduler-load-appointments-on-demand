475bf2f0401cedc607f5e6cd8e8d0ae0bcdf4990
Venkateshwaran Saravanakumar <venkateshwaran.s@syncfusion.com>
2023-05-31 16:09:00 +0530

    Documentation(SE-104579):Created Individual Sample for Load on Demand topic in Scheduler.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SchedulerLoadOnDemand.Data;
+using Syncfusion.Blazor;
+using Microsoft.EntityFrameworkCore;
+using SchedulerLoadOnDemand.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + Directory.GetCurrentDirectory() + "\\AppData\\AppointmentDataDb.mdf;Integrated Security=True";
+builder.Services.AddDbContext<AppointmentDataContext>(opts => opts.UseSqlServer(connectionString));
+
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<AppointmentDataService>();
+builder.Services.AddScoped<AppointmentDataAdaptor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapControllers();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
